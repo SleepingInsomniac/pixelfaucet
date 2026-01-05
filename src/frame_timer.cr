89196c777731b612549f59f1e86785eb1d2c101a
Alex Clink <alexclink@gmail.com>
2026-01-05 13:17:41 -0500

    Use Interval with frame_timer

--- src/frame_timer.cr
+++ src/frame_timer.cr
@@ -2,21 +2,16 @@ module PF
   # Manage the timing of frames in an animation
   class FrameTimer
     getter frame : Int32
-    @frame_time : Float64
-    @frame_count : Int32
-    @sub_frame : Float64 = 0.0
+    getter frame_count : Int32
+    getter interval : Interval
 
     def initialize(fps : Float64, @frame_count, @frame = 0)
-      @frame_time = 1.0 / fps
+      @interval = Interval.new(1.0.seconds / fps)
     end
 
-    # Update the timing given a delta time *dt*
-    def update(dt : Float64)
-      @sub_frame += dt
-      if @sub_frame > @frame_time
-        @sub_frame = @sub_frame % @frame_time
-        @frame = (@frame + 1) % @frame_count
-      end
+    # Update the timing given a delta time
+    def update(delta_time : Time::Span)
+      @interval.update(delta_time) { @frame = (@frame + 1) % @frame_count }
     end
   end
 end
