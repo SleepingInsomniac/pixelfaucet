1c3a14c0ff41cc30542dd022ac93f16b9db4517b
Alex Clink <alexclink@gmail.com>
2026-01-10 19:35:43 -0500

    Add pause to interval

--- src/interval.cr
+++ src/interval.cr
@@ -2,11 +2,14 @@ module PF
   class Interval
     property every : Time::Span
     @interval = 0u64.milliseconds
+    getter? paused = false
 
     def initialize(@every)
     end
 
     def update(delta : Time::Span, &)
+      return if @paused
+
       @interval += delta
 
       if @interval >= @every
@@ -17,6 +20,14 @@ module PF
 
     def reset
       @interval = 0u64.milliseconds
+    end
+
+    def pause
+      @paused = true
+    end
+
+    def resume
+      @paused = false
     end
   end
 end
