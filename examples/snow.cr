85c95115f865a145d5b9e2c151ead5a197a6c517
Alex Clink <alexclink@gmail.com>
2026-01-10 23:54:18 -0500

    Use Rect#cover?, use ttl for flakes

--- examples/snow.cr
+++ examples/snow.cr
@@ -11,7 +11,7 @@ class Wind
     property position : PF2d::Vec2(Float64)
     property strength : PF2d::Vec2(Float64)
 
-    def initialize(@position, @strength = PF2d::Vec[rand(-5.0..5.0), rand(-5.0..5.0)])
+    def initialize(@position, @strength = PF2d::Vec[rand(-10.0..10.0), rand(-10.0..10.0)])
     end
   end
 
@@ -42,29 +42,23 @@ class Flake
   property position : PF2d::Vec2(Float64)
   property z_pos : Float64
   property velocity : PF2d::Vec2(Float64)
+  property age : Time::Span = 0.seconds
 
   def initialize(@position, @shape = rand(0_u8..2_u8), @z_pos = rand(0.0..1.0), velocity : PF2d::Vec2(Float64)? = nil)
     @velocity = velocity || PF2d::Vec[rand(-2.0..2.0), rand(10.0..20.0)]
-  end
-
-  def update(dt)
-    @velocity.y = @velocity.y + 5.0 * dt
-    @position += @velocity * dt
   end
 end
 
 class Snow < PF::Game
   @wind : Wind
   @flakes : Array(Flake) = [] of Flake
-  @snowfall = PF::Interval.new(100.0.milliseconds)
+  @snowfall = PF::Interval.new(10.0.milliseconds)
+  @ttl = 20.seconds
 
   def initialize(*args, **kwargs)
     super
 
     @wind = Wind.new(window.width, window.height)
-    500.times do
-      @flakes << Flake.new(position: PF2d::Vec[rand(0.0..window.width.to_f64), rand(0.0..window.height.to_f64)])
-    end
   end
 
   def update(delta_time)
@@ -76,14 +70,20 @@ class Snow < PF::Game
 
     @flakes.reject! do |flake|
       @wind.gusts.each do |gust|
-        size = @wind.step / 3
-        if flake.position > gust.position - size && flake.position < gust.position + size
-          flake.velocity = flake.velocity + gust.strength * 3 * dt
+        size = @wind.step / 2
+        area = PF2d::Rect[gust.position - size, PF2d::Vec[size, size]]
+
+        if area.covers?(flake.position)
+          flake.velocity = flake.velocity + gust.strength * dt
         end
       end
 
-      flake.update(dt)
-      flake.position.y > @window.height
+      unless flake.position.y >= window.height - 1
+        flake.velocity.y = flake.velocity.y + 3.0 * dt
+        flake.position += flake.velocity * dt
+      end
+      flake.age += delta_time
+      flake.age > @ttl
     end
   end
 
@@ -91,8 +91,17 @@ class Snow < PF::Game
     window.draw do
       window.clear(0, 0, 15)
 
+      {% if flag?(:show_wind) %}
+        @wind.gusts.each do |gust|
+          size = @wind.step / 2
+          window.draw_rect(gust.position - size, gust.position + size, PF::RGBA[255, 255, 255, 20])
+          window.fill_circle(gust.position, 2, PF::RGBA[255, 255, 255, 40])
+          window.draw_line(gust.position, gust.position + gust.strength * 4, PF::RGBA[255, 255, 255, 40])
+        end
+      {% end %}
+
       @flakes.each do |flake|
-        color = PF::Colors::White * flake.z_pos
+        color = PF::Colors::White * flake.z_pos * ((@ttl - flake.age) / (@ttl / 4)).clamp(0.0, 1.0)
         if flake.shape == 0
           window.draw_point(flake.position.to_i32, color)
         else
